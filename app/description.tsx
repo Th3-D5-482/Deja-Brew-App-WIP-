1cc552566e5e877d8be8dcca795fac49220c8b8f
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-05-18 22:28:31 +0530

    Th3_D5_482

--- app/description.tsx
+++ app/description.tsx
@@ -5,42 +5,14 @@ import React, { useState } from 'react'
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
 export default function description() {
-  const {name,price,ratings,description,subTitle,isFavorite,isCart} = useLocalSearchParams();
+  const {image,name,price,ratings,description,subTitle,isFavorite,isCart} = useLocalSearchParams();
   const [favorite,setFavorite] = useState(isFavorite ==="false");
   const [cart,setCart] = useState(isCart ==="false");
-  const imageMap = {
-  "Classic Cappuccino": require("@/assets/images/classic-cappuccino.png"),
-  "Caramel Cappuccino": require("@/assets/images/caramel-cappuccino.png"),
-  "Vanilla Cappuccino": require("@/assets/images/vanilla-cappuccino.png"),
-  "HazeInut Cappuccino": require("@/assets/images/hazeinut-cappuccino.png"),
-  "Mocha Cappuccino": require("@/assets/images/mocha-cappuccino.png"),
-  "Chocolate Cappuccino": require("@/assets/images/chocolate-cappuccino.png"),
-  "Cafè Latte": require("@/assets/images/cafe-latte.png"),
-  "Vanilla Latte": require("@/assets/images/vanilla-latte.png"),
-  "Caramel Brulée Latte": require("@/assets/images/caramel-bruless-latte.png"),
-  "Pumpkin Spice Latte": require("@/assets/images/pumpkin-spice-latte.png"),
-  "Iced Lavender Oatmilk Latte": require("@/assets/images/iced-lanvender-oatmilk-latte.png"),
-  "Peppermint Mocha Latte": require("@/assets/images/peppermint-mocha-latte.png"),
-  "Caffè Americano": require("@/assets/images/caffe-americano.png"),
-  "Iced Caffè Americanos": require("@/assets/images/iced-caffe-americano.png"),
-  "Viennese Americano": require("@/assets/images/viennese-americano.png"),
-  "Decaf Americano": require("@/assets/images/decaf-americano.png"),
-  "Americano Misto": require("@/assets/images/americano-misto.png"),
-  "Almondmilk Americano": require("@/assets/images/almondmilk-americano.png"),
-  "Espresso Macchiato": require("@/assets/images/espresso-macchiato.png"),
-  "Cortado": require("@/assets/images/cortado.png"),
-  "Flat White": require("@/assets/images/flat-white.png"),
-  "Caramel Macchiato": require("@/assets/images/caramel-macchiato.png"),
-  "Caffè Mocha": require("@/assets/images/caffe-mocha.png"),
-  "Iced Shaken Espresso": require("@/assets/images/ice-shaken-espresso.png"),
-};
-
-const imageSource = imageMap[name as keyof typeof imageMap];
   return (
     <View className ='flex-1 px-8 pt-5' style = {{backgroundColor: Colors.primary}}>
         <ScrollView showsVerticalScrollIndicator = {false}>
             <View className='h-[450px] rounded-xl'>
-              <Image source={imageSource} style={{ width: 400, height: 480, objectFit: 'contain' }} className='mx-auto' />
+              <Image source={{uri: String(image)}} style={{ width: 400, height: 480, objectFit: 'contain' }} className='mx-auto' />
                 <View className='rounded-full h-12 w-12 top-[-460px] p-1'>
                   <TouchableOpacity onPress={() => router.back()}>
                     <Ionicons name='arrow-back' color={Colors.inactiveTab} size ={32}></Ionicons>
