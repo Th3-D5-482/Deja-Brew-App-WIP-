74fb3ae6bb69f3d3c0c29bc44a9ef1ffe0f2a4f7
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-07-02 23:05:53 +0530

    Th3_D5_482

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,38 +2,38 @@ import { Colors } from '@/components/colors'
 import { Ionicons } from '@expo/vector-icons'
 import { router } from 'expo-router'
 import React, { useState } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
 
 export default function profile() {
   const [tapCount, setTapCount] = useState(1);
 
- function handleTaps() {
-  setTapCount( preCount => {
-    if (preCount === 7) {
-      router.push('/about');
-      return 1;
-    }
-    return preCount + 1;
-  })
- }
+  function handleTaps() {
+    setTapCount(preCount => {
+      if (preCount === 7) {
+        router.push('/about');
+        return 1;
+      }
+      return preCount + 1;
+    })
+  }
 
   return (
-    <View className='flex-1 px-8 pt-8' style ={{backgroundColor: Colors.primary}}>
-      <View className='h-[40px] flex flex-row'>
-        <TouchableOpacity onPress={() => router.push('/(tabs)')} className='py-1'>
-          <Ionicons name ="arrow-back" color ={Colors.inactiveTab} size ={28}/>
-        </TouchableOpacity>
-        <TouchableOpacity className='py-1 ml-1' onPress={handleTaps}>
-          <Text className='text-2xl text-white text-center font-bold'>Profile</Text>
-        </TouchableOpacity>
-      </View>
-      <View className='mt-5 h-[680px]'>
-        <Text className='text-white text-[24px] text-center font-bold underline mb-32'>Project: Lyon</Text>
-        <Image source={require('@/assets/images/cofee_cup.png')} style = {{width: 300, height: 300}}className='relative mb-32 mx-auto'/>
-        <Text className='text-white text-[24px] text-center font-bold underline mb-5'>Work In Progress</Text>
-        <Text className='text-[20px] text-center text-white'>By: Th3_D5_482</Text>
-      </View>
+    <View className='flex-1 px-8 pt-8' style={{ backgroundColor: Colors.primary }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className='h-[40px] flex flex-row'>
+          <TouchableOpacity onPress={() => router.push('/(tabs)')} className='py-1'>
+            <Ionicons name="arrow-back" color={Colors.inactiveTab} size={28} />
+          </TouchableOpacity>
+          <TouchableOpacity className='py-1 ml-1' onPress={handleTaps}>
+            <Text className='text-2xl text-white text-center font-bold'>Profile</Text>
+          </TouchableOpacity>
+        </View>
+        <Image source={require('@/assets/images/Th3_D5_482.jpeg')} style={{ width: 250, height: 250, objectFit: 'contain' }} className='mx-auto mt-10 rounded-full' />
+        <View className='border border-red-800'>
+
+        </View>
+      </ScrollView>
     </View>
   )
 }
--- app/about.tsx
+++ app/about.tsx
@@ -7,23 +7,23 @@ import { Image, Linking, Text, TouchableOpacity, View } from 'react-native'
 export default class about extends Component {
   render() {
     return (
-      <View className='flex-1 px-8 pt-8' style ={{backgroundColor: Colors.primary}}>
+      <View className='flex-1 px-8 pt-8' style={{ backgroundColor: Colors.primary }}>
         <View className='h-[40px] flex flex-row'>
-            <TouchableOpacity onPress={() => router.push('/(tabs)/profile')} className='py-1'>
-            <Ionicons name ="arrow-back" color ={Colors.inactiveTab} size ={28}/>
-            </TouchableOpacity>
-            <Text className='text-2xl text-white text-center font-bold py-1 ml-1'>About Me</Text>
+          <TouchableOpacity onPress={() => router.push('/(tabs)/profile')} className='py-1'>
+            <Ionicons name="arrow-back" color={Colors.inactiveTab} size={28} />
+          </TouchableOpacity>
+          <Text className='text-2xl text-white text-center font-bold py-1 ml-1'>About Me</Text>
         </View>
         <View className='mt-5 h-[680px]'>
-            <View className='h-[250px] w-[250px] rounded-full p-1 overflow-hidden mx-auto mt-8'>
-            <Image source ={require('@/assets/images/Th3_D5_482.jpeg')} style ={{width: 240, height: 240, objectFit: 'contain'}}/>
-            </View>
-            <Text className='text-white text-3xl text-center mt-5 font-bold'>Th3_D5_482</Text>
-            <Text className='text-xl mx-2 mt-3 text-gray-400'> "I'm an innovative software developer passionate about creating efficient, user-friendly applications. With a strong background in various programming languages, I excel at solving complex problems and turning ideas into reality. I'm committed to continuous learning and delivering top-notch solutions that exceed expectations."</Text>
-            <TouchableOpacity className='h-[50px] border border-yellow-800 rounded-xl mt-8 bg-[#efe3c8] mx-2 py-3 flex flex-row px-[30%]' onPress={() => {Linking.openURL('https://github.com/Th3-D5-482');}} >
-            <Ionicons name ="logo-github" size={32}/>
+          <View className='h-[250px] w-[250px] rounded-full p-1 overflow-hidden mx-auto mt-8'>
+            <Image source={require('@/assets/images/Th3_D5_482.jpeg')} style={{ width: 250, height: 250, objectFit: 'contain' }} />
+          </View>
+          <Text className='text-white text-3xl text-center mt-5 font-bold'>Th3_D5_482</Text>
+          <Text className='text-xl mx-2 mt-3 text-gray-400'> "I'm an innovative software developer passionate about creating efficient, user-friendly applications. With a strong background in various programming languages, I excel at solving complex problems and turning ideas into reality. I'm committed to continuous learning and delivering top-notch solutions that exceed expectations."</Text>
+          <TouchableOpacity className='h-[50px] border border-yellow-800 rounded-xl mt-8 bg-[#efe3c8] mx-2 py-3 flex flex-row px-[30%]' onPress={() => { Linking.openURL('https://github.com/Th3-D5-482'); }} >
+            <Ionicons name="logo-github" size={32} />
             <Text className='text-center text-xl font-bold my-auto ml-2'>GitHub</Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
       </View>
     )
